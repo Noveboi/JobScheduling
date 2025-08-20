28fa37c2cddfd8ab1bcff09418c67029fe50101c
George Nikolaidis <nove@novesoftware.net>
2025-08-20 22:35:38 +0300

    Add REST API example project

---
+++ JobScheduling.GettingStarted/Jobs/LogJob.cs
@@ -0,0 +1,12 @@
+﻿using Quartz;
+
+namespace JobScheduling.GettingStarted.Jobs;
+
+public class LogJob : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine($"Job {context.JobDetail.Key} says hello!");
+        return Task.CompletedTask;
+    }
+}
---
+++ JobScheduling.GettingStarted/Program.cs
@@ -0,0 +1,27 @@
+﻿using JobScheduling.GettingStarted.Jobs;
+using Quartz;
+using Quartz.Impl;
+
+var schedulerFactory = new StdSchedulerFactory();
+var scheduler = await schedulerFactory.GetScheduler();
+
+await scheduler.Start();
+
+var job = JobBuilder.Create<LogJob>()
+    .WithIdentity("log-job", "main-group")
+    .Build();
+
+var trigger = TriggerBuilder.Create()
+    .WithIdentity("every-second", "main-group")
+    .StartNow()
+    .WithSimpleSchedule(x => x
+        .WithIntervalInSeconds(1)
+        .RepeatForever())
+    .Build();
+
+await scheduler.ScheduleJob(job, trigger);
+
+// Wait a bit before terminating.
+await Task.Delay(10 * 1000);
+
+await scheduler.Shutdown(waitForJobsToComplete: true);
