afe56abffbe8ea1cd6cbf1f348b256a6bff1906d
George Nikolaidis <nove@novesoftware.net>
2025-09-04 10:41:18 +0300

    Add health checks to DI

--- C - JobScheduling.Persistence/Program.cs
+++ C - JobScheduling.Persistence/Program.cs
@@ -1,7 +1,12 @@
+using JobScheduling.Persistence.Application;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<ApplicationDbContext>(o => o
     .UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
