549270be15eaa2732d1165ba7024aa18a556a82a
George Nikolaidis <nove@novesoftware.net>
2025-09-04 10:39:23 +0300

    Add `ApplicationDbContext`

---
+++ C - JobScheduling.Persistence/Application/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using JobScheduling.Persistence.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobScheduling.Persistence.Application;
+
+/// <summary>
+/// Serves as the application's default <see cref="DbContext"/>. Nothing special here, just to help illustrate a simple web API.
+/// </summary>
+internal sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; private set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var userBuilder = modelBuilder.Entity<User>();
+
+        userBuilder.HasKey(x => x.Id);
+        userBuilder.Property(x => x.Id).ValueGeneratedNever();
+
+        userBuilder.Property(x => x.Name).HasMaxLength(50);
+    }
+}
