46698a9d535e0aa11f216e14fc53e2823f1a20d1
George Nikolaidis <nove@novesoftware.net>
2025-09-04 10:59:02 +0300

    Configure Quartz services in Program.cs

--- C - JobScheduling.Persistence/Program.cs
+++ C - JobScheduling.Persistence/Program.cs
@@ -1,12 +1,20 @@
-using JobScheduling.Persistence.Application;
-using Microsoft.EntityFrameworkCore;
+using JobScheduling.Persistence.Application.Extensions;
+using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(o => o
-    .UseSqlServer(builder.Configuration.GetConnectionString("Database")));
-
-builder.Services.AddHealthChecks();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+builder.Services.AddQuartz(options =>
+{
+    options.UsePersistentStore(persistenceOptions =>
+    {
+        persistenceOptions.UseSqlServer(builder.Configuration.GetDatabaseConnectionString());
+        
+        persistenceOptions.UseProperties = true;
+        persistenceOptions.UseSystemTextJsonSerializer();
+    });
+});
 
 var app = builder.Build();
 
