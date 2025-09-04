6745d8960ed01271f1f0a516d36e94899431710c
George Nikolaidis <nove@novesoftware.net>
2025-09-04 10:40:13 +0300

    Add a DB health check

---
+++ C - JobScheduling.Persistence/Endpoints/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,18 @@
+﻿using JobScheduling.Persistence.Application;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace JobScheduling.Persistence.Endpoints.Health;
+
+internal sealed class DatabaseHealthCheck(ApplicationDbContext dbContext, ILogger<DatabaseHealthCheck> log) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken ct = new())
+    {
+        log.LogInformation("Checking health of database");
+
+        var canConnect = await dbContext.Database.CanConnectAsync(ct);
+        
+        return canConnect
+            ? HealthCheckResult.Healthy()
+            : HealthCheckResult.Unhealthy();
+    }
+}
