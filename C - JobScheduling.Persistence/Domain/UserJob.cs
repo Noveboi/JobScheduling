e9fb805d28a5325d713e55cd31bfd0f318fb8bed
George Nikolaidis <nove@novesoftware.net>
2025-09-04 11:30:00 +0300

    Create `UserJob` domain model

---
+++ C - JobScheduling.Persistence/Domain/UserJob.cs
@@ -0,0 +1,9 @@
+﻿namespace JobScheduling.Persistence.Domain;
+
+internal sealed class UserJob
+{
+    public Guid Id { get; private init; } = Guid.CreateVersion7();
+    public required Guid UserId { get; init; }
+    public required string Key { get; init; }
+    public required string Description { get; init; }
+}
