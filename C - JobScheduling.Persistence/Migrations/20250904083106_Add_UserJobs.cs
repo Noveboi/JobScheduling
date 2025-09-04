3c04ed5de50ea6b006c5ddd8ae568533c1222e7f
George Nikolaidis <nove@novesoftware.net>
2025-09-04 11:31:40 +0300

    Add EF migration for `UserJob`

---
+++ C - JobScheduling.Persistence/Migrations/20250904083106_Add_UserJobs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobScheduling.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_UserJobs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserJob",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserJob", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserJob_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserJob_UserId",
+                table: "UserJob",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserJob");
+        }
+    }
+}
