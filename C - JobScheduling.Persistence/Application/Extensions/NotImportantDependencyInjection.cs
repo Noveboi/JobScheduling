26e972841c39bb5de5224a748c3557ce60dd65fc
George Nikolaidis <nove@novesoftware.net>
2025-09-04 10:58:42 +0300

    Delegate non-crucial DI instructions to an extension method to declutter Program.cs

---
+++ C - JobScheduling.Persistence/Application/Extensions/NotImportantDependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using JobScheduling.Persistence.Endpoints.Health;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobScheduling.Persistence.Application.Extensions;
+
+public static class NotImportantDependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(configuration.GetDatabaseConnectionString()));
+        services.AddHealthChecks().AddCheck<DatabaseHealthCheck>("Database");
+        return services;
+    }
+}
