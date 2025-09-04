5ccdaa9156462c881fd6398862932d250f3a60af
George Nikolaidis <nove@novesoftware.net>
2025-09-04 11:52:14 +0300

    Add email capabilities

---
+++ C - JobScheduling.Persistence/Application/Email/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using JobScheduling.Persistence.Domain;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
+
+namespace JobScheduling.Persistence.Application.Email;
+
+internal sealed class EmailService(IOptions<SmtpSettings> settings, ILogger<EmailService> log)
+{
+    private readonly SmtpSettings _settings = settings.Value;
+    
+    public async Task SendAsync(User to, string subject, string body, CancellationToken ct)
+    {
+        log.LogInformation("Sending email to {userName}", to.Name);
+        
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Job Scheduling!", "noreply@novesoftware.net"));
+        message.To.Add(new MailboxAddress(name: to.Name, address: to.EmailAddress));
+
+        message.Subject = subject;
+        message.Body = new TextPart(TextFormat.Plain)
+        {
+            Text = body
+        };
+
+        using var client = new MailKit.Net.Smtp.SmtpClient();
+        await client.ConnectAsync(
+            host: _settings.Host,
+            port: _settings.Port,
+            useSsl: _settings.EnableSsl,
+            cancellationToken: ct);
+
+        if (!string.IsNullOrWhiteSpace(_settings.Username))
+        {
+            await client.AuthenticateAsync(
+                userName: _settings.Username,
+                password: _settings.Password,
+                cancellationToken: ct);
+        }
+
+        await client.SendAsync(message, ct);
+        await client.DisconnectAsync(quit: true, ct);
+    }
+}
---
+++ C - JobScheduling.Persistence/Application/Email/SmtpSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace JobScheduling.Persistence.Application.Email;
+
+internal sealed class SmtpSettings
+{
+    public required string Host { get; init; }
+    public required int Port { get; init; }
+    public bool EnableSsl { get; init; }
+    public string? Username { get; init; }
+    public string? Password { get; init; }
+}
