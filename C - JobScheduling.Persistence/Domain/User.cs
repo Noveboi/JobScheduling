624948ee24777da6f87fac36b594f56b8ef5b9aa
George Nikolaidis <nove@novesoftware.net>
2025-09-04 11:30:10 +0300

    Add relationship between `User` and `UserJob`

--- C - JobScheduling.Persistence/Domain/User.cs
+++ C - JobScheduling.Persistence/Domain/User.cs
@@ -4,4 +4,6 @@ internal sealed class User
 {
     public Guid Id { get; private init;  } = Guid.CreateVersion7();
     public required string Name { get; set; }
+
+    public List<UserJob> Jobs { get; set; } = [];
 }
