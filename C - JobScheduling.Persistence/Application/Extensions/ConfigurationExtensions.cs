4115bdbde02716ffce14aaa8a86f920d55aa0502
George Nikolaidis <nove@novesoftware.net>
2025-09-04 10:58:17 +0300

    Add `IConfiguration` extension methods

---
+++ C - JobScheduling.Persistence/Application/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace JobScheduling.Persistence.Application.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static string GetDatabaseConnectionString(this IConfiguration configuration)
+    {
+        var conn = configuration.GetConnectionString("Database");
+
+        return !string.IsNullOrWhiteSpace(conn) 
+            ? conn 
+            : throw new InvalidOperationException("Expected the `Database` connection string to exist.");
+    }
+}
